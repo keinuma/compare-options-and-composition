749a05152a369f46b56f11582346010897dd4540
keinuma <nununu.mono@gmail.com>
2019-12-02 18:29:36 +0900

    Add sample code for composition API

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <div class="spacer-top">
+      <button
+        class="btn btn-primary"
+        @click="handleOpenCompositionModal">
+        composition modal
+      </button>
+      <button
+        class="btn btn-primary spacer-left"
+        @click="handleOpenOptionsModal">
+        options modal
+      </button>
+    </div>
+    <modal-composition
+      :show-modal="showCompositionModal"
+      @onCloseModal="handleCloseCompositionModal"
+    >
+      <h2 class="modal-heading">Composition API</h2>
+    </modal-composition>
+    <modal-options
+      :show-modal="showOptionsModal"
+      @onCloseModal="handleCloseOptionsModal"
+    >
+      <h2 class="modal-heading">Options API</h2>
+    </modal-options>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import ModalComposition from '@/components/ModalComposition.vue'
+import ModalOptions from '@/components/ModalOptions.vue'
+
+export default Vue.extend({
+  name: 'HelloWorld',
+  components: { ModalComposition, ModalOptions },
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      showCompositionModal: false,
+      showOptionsModal: false
+    }
+  },
+  methods: {
+    handleOpenCompositionModal() {
+      this.showCompositionModal = true
+    },
+    handleOpenOptionsModal() {
+      this.showOptionsModal = true
+    },
+    handleCloseCompositionModal() {
+      this.showCompositionModal = false
+    },
+    handleCloseOptionsModal() {
+      this.showOptionsModal = false
+    }
+  }
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.spacer-left {
+  margin-left: 8px;
+}
+.spacer-top {
+  margin-top: 16px;
+}
+</style>
---
+++ src/components/ModalOptions.vue
@@ -0,0 +1,107 @@
+<template>
+  <div>
+    <div :class="{ 'modal-overlay': showModal }">
+      <div
+        v-if="showModal"
+        class="modal-isshow modal-open"
+      >
+        <div class="modal-container">
+          <button class="close-btn btn btn-outline-info" @click="closeModal">閉じる</button>
+          <slot></slot>
+          <div>
+            <div v-show="isLastDate" class="alert alert-danger" role="alert">
+              <p class="alert-last-date">
+                クリスマスになりました。<br />
+                アドベントカレンダーの最終日です。
+              </p>
+            </div>
+            <p>この記事はギルドワークスアドベントカレンダーの{{ date }}日目の記事です。</p>
+            <button type="button" class="btn btn-info" @click="countUpDate" :disabled="isLastDate">次の日</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'ModalOptions',
+  props: {
+    showModal: {
+      type: Boolean,
+      required: true,
+      default: false
+    }
+  },
+  data() {
+    return {
+      date: 1
+    }
+  },
+  computed: {
+    isLastDate() {
+      return this.date === 25
+    }
+  },
+  methods: {
+    countUpDate() {
+      if (this.isLastDate) {
+        return
+      }
+      this.date++
+    },
+    closeModal(e: Event) {
+      e.stopPropagation()
+      this.date = 1
+      this.$emit("onCloseModal")
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.modal-overlay {
+  pointer-events: auto;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: rgba(51,51,51,0.5);
+  z-index: 100;
+
+  .modal-isshow {
+    width: 70%;
+    box-shadow: none;
+    .modal-container {
+      padding: 24px;
+      margin: 0 auto;
+      border-radius: 5px;
+      background-color: #fff;
+      position: relative;
+
+      .alert-last-date {
+        margin: 16px;
+      }
+
+      .close-btn {
+        position: absolute;
+        top: 5px;
+        right: 10px;
+      }
+
+      .modal-heading {
+        margin-bottom: 24px;
+        color: #2c3e50;
+        font-size: 16px;
+      }
+    }
+  }
+}
+</style>
